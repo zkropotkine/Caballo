ddcb3387bcf718324e572042133cb12857d9d1ec
zkropotkine <s.daniel.rodriguez@gmail.com>
2014-03-30 23:04:27 -0600

    Commit

---
+++ src/main/java/com/cordyceps/caballo/App.java
@@ -0,0 +1,13 @@
+package com.cordyceps.caballo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
